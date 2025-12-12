ce1d53bd85c8095274e5d959d04016056fdbf12a
Odair Gazarolla <odair.gazarolla@gmail.com>
2025-12-12 15:54:45 -0300

    Add components directly on App.tsx for tests purpose

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import './App.css'
 
+
+// Component for initial tests
+function CustomButton() {
+  return (
+    <button>Custom Button!</button>
+  )
+}
+
+const customButton2 = () => {
+    return (
+      <button>Custom Button 2!</button>
+    )
+  }
+
+const customButton3 = (
+    <button>Custom Button 3!</button>
+  );
+
+// ----- End of test Components -----
+
+
 function App() {
   return (
     <>
       <div>
         <p>Let's start from the <h1>Stone Age</h1></p>
+        <div className='buttons-box'>
+          <CustomButton />
+          { customButton2() }
+          { customButton3 }
+        </div>
       </div>
     </>
   )
